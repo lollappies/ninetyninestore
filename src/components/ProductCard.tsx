6145ceb96735c50aee6930993ea255b97107d4cc
lollappies <lifiaria88@gmail.com>
2026-05-14 14:51:29 +0700

    update error

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import { Heart, ShoppingBag } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { useCustomToast } from './CustomToast';
 import { Product, allProducts } from '../utils/data';
-import { useLanguage } from '../context/LanguageContext';
+export const useLanguage = useLang;
 
 interface ProductCardProps {
   product: Product;
