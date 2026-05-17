4a73a950fc7b42413afcd841f563138b672b909f
lollappies <lifiaria88@gmail.com>
2026-05-17 20:16:51 +0700

    A+ la

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   build: {
     rollupOptions: {
       output: {
         inlineDynamicImports: false,
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
-  },
-  
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-
-  export default defineConfig({
-  plugins:[react()]
-  })
+  }
+})
